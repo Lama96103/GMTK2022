26c0fc8fd1e57b22d8ce258485ee987d44cc8598
Lama96103 <larswunderlich12@gmail.com>
2022-07-16 16:53:52 +0200

    Add Basic Menu

---
+++ Nodes/WorldController.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public class WorldController : Spatial
+{
+    public static WorldController Instance {get;private set;}
+
+    private LevelController currentLevel;
+
+    public override void _Ready()
+    {
+        WorldController.Instance = this;
+        LoadLevel("res://Nodes/Level/Level_001.tscn");
+    }
+
+    public void LoadLevel(NodePath path)
+    {
+        if(currentLevel != null)
+        {
+            this.RemoveChild(currentLevel);
+            currentLevel.QueueFree();
+        }
+
+        PackedScene scene = (PackedScene)ResourceLoader.Load(path);
+        currentLevel = scene.Instance<LevelController>();
+        this.AddChild(currentLevel);
+    }
+
+    public void LoadMainMenu()
+    {
+        this.GetTree().ChangeScene("res://Nodes/Menu/MainMenu.tscn");
+    }
+
+    public void LoadDeathMenu()
+    {
+        this.GetTree().ChangeScene("res://Nodes/Menu/MainMenu.tscn");
+    }
+
+}
