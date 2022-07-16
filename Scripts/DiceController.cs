5b639d027696d9a90f7583ad4abdb74dc8e3c3ab
Tom <korttomp@yahoo.de>
2022-07-16 12:02:39 +0200

    ADD PlaySound() in diceController

--- Scripts/DiceController.cs
+++ Scripts/DiceController.cs
@@ -65,7 +65,39 @@ public abstract class DiceController : Spatial
 
         progress = 0;
         isRolling = true;
+
+        PlaySound();
     }
 
+    private void PlaySound()
+    {
+        String path = "res://Sounds and Music/Sounds/";
+        int lastIndex = CurrentLevel.GetParent().GetChildCount();
+        AudioStreamPlayer audioPlayer = CurrentLevel.GetParent().GetChild<AudioStreamPlayer>(lastIndex - 1);
+        if(ActiveEffect() == null)
+        {
+            String[] diceSounds = new String[4];
+            for(int i = 0; i < diceSounds.Length; i++)
+            {
+                diceSounds[i] = "Dice 0" + (i+1) + ".wav";
+            }
+            Random random = new Random();
+            int selection = random.Next(0, diceSounds.Length);
+            AudioStreamSample audioStream = ResourceLoader.Load<AudioStreamSample>(path + diceSounds[selection]);
+            audioPlayer.Stream = audioStream;
+            audioPlayer.Play();
+        }
+        if(ActiveEffect() == "FireEffect")
+        {           
+            AudioStreamSample audioStream = ResourceLoader.Load<AudioStreamSample>(path + "mixkit-fire-swoosh-burning-2s.wav");
+            audioPlayer.Stream = audioStream;
+            audioPlayer.Play();
+        }
+    }
+
+    public String ActiveEffect()
+    {
+        return null;
+    }
     public abstract bool ExecuteTurn();
 }
